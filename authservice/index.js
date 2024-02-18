0fdd7a4a911d25a2d3ca88e5f2384b82e5bc4239
IMRANDIL <aliimranadil2@gmail.com>
2024-02-18 10:21:41 +0530

    just simulation

--- authservice/index.js
+++ authservice/index.js
@@ -4,7 +4,7 @@ const PORT = 3001;
 
 app.get('/authenticate', (req, res) => {
   // Dummy authentication logic
-  const isAuthenticated = Math.random() < 0.5; // 50% chance of success
+  const isAuthenticated = Math.random() < 0.8; // 50% chance of success
   if (isAuthenticated) {
     res.send('Authentication successful');
   } else {
